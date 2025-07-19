e0c37b53061ce1d53b2bb05c02e35996ccb75548
Mohamed Abdelhlim Saeed <MHalem_DEV@outlook.com>
2025-07-19 17:51:28 +0300

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,8 +1,13 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
-      {/* PostCard components will go here */}
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
+        {/* PostCard components will go here */}
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,8 +1,13 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Users Page</h1>
-      {/* UserCard components will go here */}
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-3xl font-bold mb-4">Users Page</h1>
+        {/* UserCard components will go here */}
+      </main>
     </div>
   );
 };
